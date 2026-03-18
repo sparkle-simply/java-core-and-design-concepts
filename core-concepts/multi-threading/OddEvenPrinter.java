87168d6bc9aded3ae5cb97e1d21f640c24c6cbcc
Simran Sharma <sharmakammo13@gmail.com>
2026-03-18 23:41:41 +0530

    multi threading concept

--- core-concepts/multi-threading/OddEvenPrinter.java
+++ core-concepts/multi-threading/OddEvenPrinter.java
@@ -1,3 +1,6 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Approach1:
  * Shared variable number keeps track of current value.
@@ -42,14 +45,64 @@ class OddEvenPrinterApproach1 {
     }
 }
 
-public class Main {
-    public static void main(String[] args) {
-        OddEvenPrinter printer = new OddEvenPrinter();
+/**
+ * Approach2:
+ * Using ExecutorService
+ */
+class OddEvenPrinterApproach2 {
+    private int number = 1;
+    private final int MAX = 20;
 
+    public synchronized void printOdd() {
+        while (number <= MAX) {
+            while (number % 2 == 0) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+
+            if (number <= MAX) {
+                System.out.println("Odd: " + number);
+                number++;
+                notifyAll();
+            }
+        }
+    }
+
+    public synchronized void printEven() {
+        while (number <= MAX) {
+            while (number % 2 != 0) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+
+            if (number <= MAX) {
+                System.out.println("Even: " + number);
+                number++;
+                notifyAll();
+            }
+        }
+    }
+}
+
+public class App {
+    public static void util(String[] args) {
+
+        OddEvenPrinterApproach1 printer = new OddEvenPrinterApproach1();
         Thread oddThread = new Thread(() -> printer.printOdd());
         Thread evenThread = new Thread(() -> printer.printEven());
-
         oddThread.start();
         evenThread.start();
+
+        OddEvenPrinterApproach2 printer = new OddEvenPrinterApproach2();
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        executor.execute(() -> printer.printOdd());
+        executor.execute(() -> printer.printEven());
+        executor.shutdown();
     }
 }
